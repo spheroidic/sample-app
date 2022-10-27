878c20c15290d88d9836ee5358534ad3712f71df
Trinidaddy <trinidad961@gmail.com>
2022-10-27 17:36:15 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { MyCalendar } from "./Components/MyCalendar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className="paragraph">This should go on the left</p>
+      <MyCalendar className="calendar" />
+    </>
   );
 }
 
---
+++ src/Components/MyCalendar.js
@@ -0,0 +1,10 @@
+import Calendar from "@toast-ui/react-calendar";
+import "@toast-ui/calendar/dist/toastui-calendar.min.css";
+
+export const MyCalendar = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+    );
+};
